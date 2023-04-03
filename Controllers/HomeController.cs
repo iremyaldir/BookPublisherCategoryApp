88dccfab75edeb55a3c62a72269cf271d3cc9bfb
İrem Yaldır <115476586+iremyaldir@users.noreply.github.com>
2023-04-03 05:18:13 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using BPC.Data;
+using BPC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BPC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //var db = new ApplicationDbContext();
+            //var Books = db.Books.Where(x => x.Name).OrderbyDescending(a => a.Price);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BPC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
